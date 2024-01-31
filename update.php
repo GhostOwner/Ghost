50e405e7096f6e2b575307211d4af59ce44be8b8
GhostOwner <116358688+GhostOwner@users.noreply.github.com>
2024-01-31 23:48:28 +0200

    Create update.php

---
+++ update.php
@@ -0,0 +1,10 @@
+{
+  "Visibility": "VISIBLE",
+  "Title": "Anubis",
+  "Description": "Test Message",
+  "MainTextButton": "Update",
+  "Cancelable": "true",
+  "CancelButtonText": "Cancel",
+  "DownloadLink": "www.google.com",
+  "Version": "2.1"
+}
